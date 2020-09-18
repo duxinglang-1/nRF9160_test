ba864c9bec77182bda596ac5ef2892c8747a2454
xiebiao <biao.x@augustint.com>
2020-09-18 09:29:29 +0800

    修改自定义按键响应的初始化以及处理。

--- uart_ble/src/key/key.c
+++ uart_ble/src/key/key.c
@@ -3,88 +3,49 @@
 #include <stdio.h>
 #include <zephyr.h>
 #include <sys/printk.h>
-#include <dk_buttons_and_leds.h>
+#include <soc.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
+#include <nrfx.h>
 #include "key.h"
-#include "Max20353.h"
+//#include "Max20353.h"
 
-#define TIMER_FOR_LONG_PRESSED 1000
+static u8_t flag;
+static u32_t keycode;
+static u32_t keytype;
 
-#define KEY_DOWN 		1
-#define KEY_UP 			0
-#define KEY_LONG_PRESS	2
+static KEY_STATUS state;
+static button_handler_t button_handler_cb;
+static atomic_t my_buttons;
+static sys_slist_t button_handlers;
 
-#define KEY_1			BIT(0)
-#define KEY_2			BIT(1)
-#define KEY_3			BIT(2)
-#define KEY_4			BIT(3)
-#define KEY_SOS			BIT(4)	//SOS 26
-#define KEY_PWR			BIT(5)	//power 15
+#define KEY_SOS			BIT(0)
+#define KEY_PWR			BIT(1)
+static const gpio_pin button_pins[] = 
+{
+	{DT_ALIAS_SW0_GPIOS_CONTROLLER, 26},
+	{DT_ALIAS_SW0_GPIOS_CONTROLLER, 15},
+};
+
+static struct device *button_devs[ARRAY_SIZE(button_pins)];
+static struct gpio_callback gpio_cb;
+static struct k_spinlock lock;
+static struct k_delayed_work buttons_scan;
+static struct k_mutex button_handler_mut;
+static struct k_timer g_long_press_timer_id;
 
 extern bool lcd_sleep_in;
 extern bool lcd_sleep_out;
 extern bool lcd_is_sleeping;
 extern bool sys_pwr_off;
 
-static struct k_timer g_long_press_timer_id;
-static u8_t flag;
-static u32_t keycode;
-static u32_t keytype;
-
-static void key_event_handler(uint8_t key_code, uint8_t key_type)
+static void key_event_handler(u8_t key_code, u8_t key_type)
 {
-	//printk("key_code:%d, key_type:%d, KEY_1:%d,KEY_2:%d,KEY_3:%d,KEY_4:%d,KEY_5:%d,KEY_6:%d\n", 
-	//						key_code, key_type,
-	//						KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6);
+	printk("key_code:%d, key_type:%d, KEY_SOS:%d,KEY_PWR:%d\n", key_code, key_type,	KEY_SOS, KEY_PWR);
 
 	switch(key_code)
 	{
-	case KEY_1:
-		switch(key_type)
-		{
-		case KEY_DOWN:
-			break;
-		case KEY_UP:
-			break;
-		case KEY_LONG_PRESS:
-			break;
-		}
-		break;
-
-	case KEY_2:
-		switch(key_type)
-		{
-		case KEY_DOWN:
-			break;
-		case KEY_UP:
-			break;
-		case KEY_LONG_PRESS:
-			break;
-		}		
-		break;
-
-	case KEY_3:
-		switch(key_type)
-		{
-		case KEY_DOWN:
-			break;
-		case KEY_UP:
-			break;
-		case KEY_LONG_PRESS:
-			break;
-		}		
-		break;
-
-	case KEY_4:
-		switch(key_type)
-		{
-		case KEY_DOWN:
-			break;
-		case KEY_UP:
-			break;
-		case KEY_LONG_PRESS:
-			break;
-		}
-		break;
 	case KEY_SOS:
 		switch(key_type)
 		{
@@ -145,6 +106,274 @@ static void long_press_timer_handler(struct k_timer *timer)
     key_event_handler(keycode, KEY_LONG_PRESS);
 }
 
+static int callback_ctrl(bool enable)
+{
+	int err = 0;
+
+	/* This must be done with irqs disabled to avoid pin callback
+	 * being fired before others are still not activated.
+	 */
+	for(size_t i = 0; (i < ARRAY_SIZE(button_pins)) && !err; i++)
+	{
+		if(enable)
+		{
+			err = gpio_pin_enable_callback(button_devs[i], button_pins[i].number);
+		}
+		else
+		{
+			err = gpio_pin_disable_callback(button_devs[i], button_pins[i].number);
+		}
+	}
+
+	return err;
+}
+
+static u32_t get_buttons(void)
+{
+	u32_t ret = 0;
+	
+	for(size_t i = 0; i < ARRAY_SIZE(button_pins); i++)
+	{
+		u32_t val;
+
+		if(gpio_pin_read(button_devs[i], button_pins[i].number, &val))
+		{
+			printk("Cannot read gpio pin");
+			return 0;
+		}
+		
+		if((val && !IS_ENABLED(CONFIG_DK_LIBRARY_INVERT_BUTTONS)) ||
+		    (!val && IS_ENABLED(CONFIG_DK_LIBRARY_INVERT_BUTTONS)))
+		{
+			ret |= 1U << i;
+		}
+	}
+
+	return ret;
+}
+
+static void button_handlers_call(u32_t button_state, u32_t has_changed)
+{
+	struct button_handler *handler;
+
+	if(button_handler_cb != NULL)
+	{
+		button_handler_cb(button_state, has_changed);
+	}
+
+	if(IS_ENABLED(CONFIG_DK_LIBRARY_DYNAMIC_BUTTON_HANDLERS))
+	{
+		k_mutex_lock(&button_handler_mut, K_FOREVER);
+		SYS_SLIST_FOR_EACH_CONTAINER(&button_handlers, handler, node)
+		{
+			handler->cb(button_state, has_changed);
+		}
+		k_mutex_unlock(&button_handler_mut);
+	}
+}
+
+static void buttons_scan_fn(struct k_work *work)
+{
+	static u32_t last_button_scan;
+	static bool initial_run = true;
+	u32_t button_scan;
+
+	button_scan = get_buttons();
+	atomic_set(&my_buttons, (atomic_val_t)button_scan);
+
+	if(!initial_run)
+	{
+		if(button_scan != last_button_scan)
+		{
+			u32_t has_changed = (button_scan ^ last_button_scan);
+			button_handlers_call(button_scan, has_changed);
+		}
+	}
+	else
+	{
+		initial_run = false;
+	}
+
+	last_button_scan = button_scan;
+
+	if (button_scan != 0)
+	{
+		int err = k_delayed_work_submit(&buttons_scan, CONFIG_DK_LIBRARY_BUTTON_SCAN_INTERVAL);
+		if(err)
+		{
+			printk("Cannot add work to workqueue");
+		}
+	}
+	else
+	{
+		/* If no button is pressed module can switch to callbacks */
+		int err = 0;
+
+		k_spinlock_key_t key = k_spin_lock(&lock);
+
+		switch (state)
+		{
+		case STATE_SCANNING:
+			state = STATE_WAITING;
+			err = callback_ctrl(true);
+			break;
+
+		default:
+			__ASSERT_NO_MSG(false);
+			break;
+		}
+
+		k_spin_unlock(&lock, key);
+
+		if(err)
+		{
+			printk("Cannot enable callbacks");
+		}
+	}
+}
+
+static int set_trig_mode(int trig_mode)
+{
+	int err = 0;
+	int flags = (IS_ENABLED(CONFIG_DK_LIBRARY_INVERT_BUTTONS) ?
+		(GPIO_PUD_PULL_UP | GPIO_INT_ACTIVE_LOW) :
+		(GPIO_PUD_PULL_DOWN | GPIO_INT_ACTIVE_HIGH));
+	flags |= (GPIO_DIR_IN | GPIO_INT | trig_mode);
+
+	for(size_t i = 0; (i < ARRAY_SIZE(button_pins)) && !err; i++)
+	{
+		err = gpio_pin_configure(button_devs[i], button_pins[i].number, flags);
+	}
+
+	return err;
+}
+
+static void read_buttons(u32_t *button_state, u32_t *has_changed)
+{
+	static u32_t last_state;
+	u32_t current_state = atomic_get(&my_buttons);
+
+	if(button_state != NULL)
+	{
+		*button_state = current_state;
+	}
+
+	if(has_changed != NULL)
+	{
+		*has_changed = (current_state ^ last_state);
+	}
+
+	last_state = current_state;
+}
+
+static void button_pressed(struct device *gpio_dev, struct gpio_callback *cb, u32_t pins)
+{
+	k_spinlock_key_t key = k_spin_lock(&lock);
+
+	/* Disable GPIO interrupt */
+	int err = callback_ctrl(false);
+
+	if(err)
+	{
+		printk("Cannot disable callbacks");
+	}
+
+	switch (state)
+	{
+	case STATE_WAITING:
+		state = STATE_SCANNING;
+		k_delayed_work_submit(&buttons_scan, 1);
+		break;
+
+	case STATE_SCANNING:
+	default:
+		/* Invalid state */
+		__ASSERT_NO_MSG(false);
+		break;
+	}
+
+	k_spin_unlock(&lock, key);
+}
+
+static int buttons_init(button_handler_t button_handler)
+{
+	int err;
+
+	button_handler_cb = button_handler;
+
+	if(IS_ENABLED(CONFIG_DK_LIBRARY_DYNAMIC_BUTTON_HANDLERS))
+	{
+		k_mutex_init(&button_handler_mut);
+	}
+
+	for(size_t i = 0; i < ARRAY_SIZE(button_pins); i++)
+	{
+		button_devs[i] = device_get_binding(button_pins[i].port);
+		if (!button_devs[i])
+		{
+			printk("Cannot bind gpio device");
+			return -ENODEV;
+		}
+
+		err = gpio_pin_configure(button_devs[i], button_pins[i].number, GPIO_DIR_IN | GPIO_PUD_PULL_UP);
+		if(err)
+		{
+			printk("Cannot configure button gpio");
+			return err;
+		}
+	}
+
+	err = set_trig_mode(GPIO_INT_LEVEL);
+	if(err)
+	{
+		printk("Cannot set interrupt mode");
+		return err;
+	}
+
+	u32_t pin_mask = 0;
+
+	for (size_t i = 0; i < ARRAY_SIZE(button_pins); i++)
+	{
+		/* Module starts in scanning mode and will switch to
+		 * callback mode if no button is pressed.
+		 */
+		err = gpio_pin_disable_callback(button_devs[i], button_pins[i].number);
+		if(err)
+		{
+			printk("Cannot disable callbacks()");
+			return err;
+		}
+
+		pin_mask |= BIT(button_pins[i].number);
+	}
+
+	gpio_init_callback(&gpio_cb, button_pressed, pin_mask);
+
+	for (size_t i = 0; i < ARRAY_SIZE(button_pins); i++)
+	{
+		err = gpio_add_callback(button_devs[i], &gpio_cb);
+		if(err)
+		{
+			printk("Cannot add callback");
+			return err;
+		}
+	}
+
+	k_delayed_work_init(&buttons_scan, buttons_scan_fn);
+
+	state = STATE_SCANNING;
+
+	err = k_delayed_work_submit(&buttons_scan, 0);
+	if(err)
+	{
+		printk("Cannot add work to workqueue");
+		return err;
+	}
+
+	read_buttons(NULL, NULL);
+
+	return 0;
+}
 
 void key_init(void)
 {
@@ -152,7 +381,7 @@ void key_init(void)
 
 	printk("key_init\n");
 	
-	err = dk_buttons_init(button_handler);
+	err = buttons_init(button_handler);
 	if (err)
 	{
 		printk("Could not initialize buttons, err code: %d\n", err);
--- uart_ble/src/key/key.h
+++ uart_ble/src/key/key.h
@@ -1,2 +1,85 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+
+#include <zephyr/types.h>
+#include <sys/slist.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DK_NO_BTNS_MSK   (0)
+#define DK_BTN1          0
+#define DK_BTN2          1
+#define DK_BTN1_MSK      BIT(DK_BTN1)
+#define DK_BTN2_MSK      BIT(DK_BTN2)
+#define DK_ALL_BTNS_MSK  (DK_BTN1_MSK | DK_BTN2_MSK)
+
+#define KEY_DOWN 		1
+#define KEY_UP 			0
+#define KEY_LONG_PRESS	2
+
+#define TIMER_FOR_LONG_PRESSED 3000
+
+typedef struct{
+	const char * const port;
+	const u8_t number;
+}gpio_pin;
+
+typedef enum 
+{
+	STATE_WAITING,
+	STATE_SCANNING,
+}KEY_STATUS;
+
+/**
+ * @typedef button_handler_t
+ * @brief Callback that is executed when a button state change is detected.
+ *
+ * @param button_state Bitmask of button states.
+ * @param has_changed Bitmask that shows which buttons have changed.
+ */
+typedef void (*button_handler_t)(u32_t button_state, u32_t has_changed);
+
+/** Button handler list entry. */
+struct button_handler {
+	button_handler_t cb; /**< Callback function. */
+	sys_snode_t node; /**< Linked list node, for internal use. */
+};
+
+/** @brief Initialize the library to read the button state.
+ *
+ *  @param  button_handler Callback handler for button state changes.
+ *
+ *  @retval 0           If the operation was successful.
+ *                      Otherwise, a (negative) error code is returned.
+ */
+int dk_buttons_init(button_handler_t button_handler);
+
+/** @brief Read current button states.
+ *
+ *  @param button_state Bitmask of button states.
+ *  @param has_changed Bitmask that shows which buttons have changed.
+ */
+void dk_read_buttons(u32_t *button_state, u32_t *has_changed);
+
+/** @brief Get current button state from internal variable.
+ *
+ *  @return Bitmask of button states.
+ */
+u32_t dk_get_buttons(void);
+
+/** @brief Set value of LED pins as specified in one bitmask.
+ *
+ *  @param  leds Bitmask that defines which LEDs to turn on and off.
+ *
+ *  @retval 0           If the operation was successful.
+ *                      Otherwise, a (negative) error code is returned.
+ */
 
 
+#ifdef __cplusplus
+}
+#endif
+
+#endif/*__KEY_H__*/

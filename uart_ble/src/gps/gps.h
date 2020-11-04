b2a4454addeb1334ae2e0a898e0a9ba4939d210c
xiebiao <biao.x@augustint.com>
2020-11-04 15:28:37 +0800

    1.增加GPS部分有关APP指令获取定位信息的一些接口函数。 2.增加了GPS对应的关闭操作。

---
+++ uart_ble/src/gps/gps.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+#include <nrf_socket.h>
+#include <net/socket.h>
+#include <stdio.h>
+
+extern bool got_first_fix;
+extern nrf_gnss_data_frame_t last_fix;

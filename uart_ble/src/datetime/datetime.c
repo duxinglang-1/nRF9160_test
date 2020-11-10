89941eeb65a78b644a459ff618c602bfe1b7b85f
xiebiao <biao.x@augustint.com>
2020-11-10 19:03:37 +0800

    去掉缺少字库的提示。

--- uart_ble/src/datetime/datetime.c
+++ uart_ble/src/datetime/datetime.c
@@ -378,14 +378,6 @@ void IdleShowSystemWeek(void)
 #endif
 
 	GetSystemWeekStrings(str_week);
-
-	//xb add 2020-11-06
-	if(global_settings.language == LANGUAGE_CHN)
-		strcpy(str_week,"It has no chinese font!");
-	else if(global_settings.language == LANGUAGE_JPN)
-		strcpy(str_week,"It has no japanese font!");
-	//xb end
-	
 	LCD_MeasureString(str_week,&w,&h);
 	x = (LCD_WIDTH > w) ? (LCD_WIDTH-w)/2 : 0;
 	y = IDLE_WEEK_SHOW_Y;

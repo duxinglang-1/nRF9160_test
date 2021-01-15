d590e8fb16ca0e7fd45b1a8f82a5ef3513a7210d
xiebiao <biao.x@augustint.com>
2021-01-15 13:49:50 +0800

    打开NTC电池温度检测。

--- AURI/src/pmu/max20353.h
+++ AURI/src/pmu/max20353.h
@@ -8,7 +8,7 @@
 //#define MOTOR_TYPE_LRA	//线性马达
 
 #define BATTERY_SOC_GAUGE	//xb add 20201124 电量计功能的代码
-//#define BATTERT_NTC_CHECK	//xb add 20210106 电池增加NTC温度检测
+#define BATTERT_NTC_CHECK	//xb add 20210106 电池增加NTC温度检测
 
 typedef enum
 {

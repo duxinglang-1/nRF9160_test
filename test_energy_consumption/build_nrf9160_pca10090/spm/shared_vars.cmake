a637be0fe6a05cea9245b3ee177072103a3fd040
xiebiao <biao.x@augustint.com>
2021-04-10 16:42:40 +0800

    删除多余的文件。

--- test_energy_consumption/build_nrf9160_pca10090/spm/shared_vars.cmake
+++
@@ -1,6 +0,0 @@
-set(spm_KERNEL_HEX_NAME zephyr.hex)
-set(spm_KERNEL_ELF_NAME zephyr.elf)
-list(APPEND spm_BUILD_BYPRODUCTS D:/software/nrf9160/my_projects/my_test/test_energy_consumption/build_nrf9160_pca10090/spm/zephyr/zephyr.hex)
-list(APPEND spm_BUILD_BYPRODUCTS D:/software/nrf9160/my_projects/my_test/test_energy_consumption/build_nrf9160_pca10090/spm/zephyr/zephyr.elf)
-list(APPEND spm_BUILD_BYPRODUCTS
-    D:/software/nrf9160/my_projects/my_test/test_energy_consumption/build_nrf9160_pca10090/spm/libspmsecureentries.a)

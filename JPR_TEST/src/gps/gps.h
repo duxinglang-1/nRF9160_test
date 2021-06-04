c7f96a41cbff45e851d75432f10b30d510f4ee8f
xiebiao <biao.x@augustint.com>
2021-06-04 16:05:51 +0800

    1.调整GPS功能的入口及数据输出。 2.增加通讯控制模块。

--- JPR_TEST/src/gps/gps.h
+++ JPR_TEST/src/gps/gps.h
@@ -5,9 +5,8 @@
  */
 
 #include <zephyr.h>
-#include <nrf_socket.h>
-#include <net/socket.h>
 #include <stdio.h>
+#include <drivers/gps.h>
 
 extern bool app_gps_on;
 extern bool app_gps_off;
@@ -15,6 +14,10 @@ extern bool app_gps_off;
 extern bool ble_wait_gps;
 extern bool sos_wait_gps;
 extern bool fall_wait_gps;
+extern bool location_wait_gps;
+extern bool test_gps_flag;
 
-extern void gps_init(void);
+extern u8_t gps_test_info[256];
+
+extern void GPS_init(struct k_work_q *work_q);
 extern void GPSMsgProcess(void);

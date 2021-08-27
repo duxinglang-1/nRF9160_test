c121ce6a0c33d19f006e009c22bed38565a3bf5a
xiebiao <biao.x@augustint.com>
2021-08-27 11:34:38 +0800

    1.修改关机提示界面，并增加触摸功能。 2.修改睡眠测试界面，增加睡眠总时间。

--- JPR_TEST/src/screen/screen.c
+++ JPR_TEST/src/screen/screen.c
@@ -332,19 +332,15 @@ void IdleUpdateBatSoc(void)
 
 void IdleShowSignal(void)
 {
-#if 0
-#ifdef IMG_FONT_FROM_FLASH
-	u32_t img_addr[5] = {IMG_SIG_0_ADDR,IMG_SIG_1_ADDR,IMG_SIG_2_ADDR,IMG_SIG_3_ADDR,IMG_SIG_4_ADDR};
-#else
-	unsigned char *img[5] = {IMG_SIG_0,IMG_SIG_1,IMG_SIG_2,IMG_SIG_3,IMG_SIG_4};
-#endif
+	u8_t i;
+	
+	LCD_Fill(NB_SIGNAL_FILL_RECT_X-1,NB_SIGNAL_FILL_RECT_Y-1,(4*NB_SIGNAL_FILL_RECT_W+3*NB_SIGNAL_FILL_RECT_OFFSET_X)+2,NB_SIGNAL_FILL_RECT_H+2,BLACK);
 
-#ifdef IMG_FONT_FROM_FLASH
-	LCD_ShowImg_From_Flash(NB_SIGNAL_X, NB_SIGNAL_Y, img_addr[g_nb_sig]);
-#else
-	LCD_ShowImg(NB_SIGNAL_X, NB_SIGNAL_Y, img[g_nb_sig]);
-#endif
-#endif
+	for(i=0;i<4;i++)
+		LCD_DrawRectangle(NB_SIGNAL_FILL_RECT_X+i*(NB_SIGNAL_FILL_RECT_W+NB_SIGNAL_FILL_RECT_OFFSET_X),NB_SIGNAL_FILL_RECT_Y,NB_SIGNAL_FILL_RECT_W,NB_SIGNAL_FILL_RECT_H);
+	
+	for(i=0;i<g_nb_sig;i++)
+		LCD_Fill(NB_SIGNAL_FILL_RECT_X+i*(NB_SIGNAL_FILL_RECT_W+NB_SIGNAL_FILL_RECT_OFFSET_X),NB_SIGNAL_FILL_RECT_Y,NB_SIGNAL_FILL_RECT_W,NB_SIGNAL_FILL_RECT_H,WHITE);
 }
 
 void IdleShowBatSoc(void)
@@ -553,6 +549,7 @@ void IdleScreenProcess(void)
 		scr_msg[SCREEN_ID_IDLE].status = SCREEN_STATUS_CREATED;
 		
 		LCD_Clear(BLACK);
+		IdleShowSignal();
 		IdleShowBatSoc();
 		IdleShowDateTime();
 		IdleShowSportData();
@@ -1040,16 +1037,23 @@ void SleepScreenProcess(void)
 
 		GetSleepTimeData(&deep_sleep, &light_sleep);
 		LCD_SetFontSize(FONT_SIZE_16);
+		LCD_MeasureString("TOTAL_SLEEP:        ",&w,&h);
+		x = (w > LCD_WIDTH)? 0 : (LCD_WIDTH-w)/2;
+		y = 100;
+		LCD_ShowString(x,y,"TOTAL_SLEEP:        ");
+		sprintf(tmpbuf, "%d (min)", deep_sleep+light_sleep);
+		LCD_ShowString(x+13*FONT_SIZE_16/2, y, tmpbuf);
+
 		LCD_MeasureString("DEEP_SLEEP:        ",&w,&h);
 		x = (w > LCD_WIDTH)? 0 : (LCD_WIDTH-w)/2;
-		y = 110;
+		y = 120;
 		LCD_ShowString(x,y,"DEEP_SLEEP:        ");
 		sprintf(tmpbuf, "%d (min)", deep_sleep);
 		LCD_ShowString(x+12*FONT_SIZE_16/2, y, tmpbuf);
 
 		LCD_MeasureString("LIGHT_SLEEP:        ",&w,&h);
 		x = (w > LCD_WIDTH)? 0 : (LCD_WIDTH-w)/2;
-		y = 130;
+		y = 140;
 		LCD_ShowString(x,y,"LIGHT_SLEEP:        ");
 		sprintf(tmpbuf, "%d (min)", deep_sleep);
 		LCD_ShowString(x+13*FONT_SIZE_16/2, y, tmpbuf);
@@ -1058,17 +1062,24 @@ void SleepScreenProcess(void)
 	case SCREEN_ACTION_UPDATE:
 		GetSleepTimeData(&deep_sleep, &light_sleep);
 		LCD_SetFontSize(FONT_SIZE_16);
-		LCD_MeasureString("DEEP_SLEEP:		  ",&w,&h);
+		LCD_MeasureString("TOTAL_SLEEP:        ",&w,&h);
 		x = (w > LCD_WIDTH)? 0 : (LCD_WIDTH-w)/2;
-		y = 110;
-		LCD_ShowString(x,y,"DEEP_SLEEP: 	   ");
+		y = 100;
+		LCD_ShowString(x,y,"TOTAL_SLEEP:        ");
+		sprintf(tmpbuf, "%d (min)", deep_sleep+light_sleep);
+		LCD_ShowString(x+13*FONT_SIZE_16/2, y, tmpbuf);
+
+		LCD_MeasureString("DEEP_SLEEP:        ",&w,&h);
+		x = (w > LCD_WIDTH)? 0 : (LCD_WIDTH-w)/2;
+		y = 120;
+		LCD_ShowString(x,y,"DEEP_SLEEP:        ");
 		sprintf(tmpbuf, "%d (min)", deep_sleep);
 		LCD_ShowString(x+12*FONT_SIZE_16/2, y, tmpbuf);
 
-		LCD_MeasureString("LIGHT_SLEEP: 	   ",&w,&h);
+		LCD_MeasureString("LIGHT_SLEEP:        ",&w,&h);
 		x = (w > LCD_WIDTH)? 0 : (LCD_WIDTH-w)/2;
-		y = 130;
-		LCD_ShowString(x,y,"LIGHT_SLEEP:		");
+		y = 140;
+		LCD_ShowString(x,y,"LIGHT_SLEEP:        ");
 		sprintf(tmpbuf, "%d (min)", deep_sleep);
 		LCD_ShowString(x+13*FONT_SIZE_16/2, y, tmpbuf);
 		break;
@@ -1489,7 +1500,7 @@ void EnterIdleScreen(void)
 #ifdef CONFIG_FOTA_DOWNLOAD
 	Key_Event_register_Handler(fota_start, EnterIdleScreen);	
 #else
-	Key_Event_register_Handler(EnterHRScreen, EnterIdleScreen);
+	Key_Event_register_Handler(APPStartPPG, EnterIdleScreen);
 #endif
 }
 
@@ -1692,12 +1703,14 @@ void EnterFOTAScreen(void)
 }
 void poweroff_leftkeyfunc(void)
 {
-	EnterIdleScreen();
+	LOG_INF("[%s]\n", __func__);
+	key_pwroff_flag = true;
 }
 
 void poweroff_rightkeyfunc(void)
 {
-	key_pwroff_flag = true;
+	LOG_INF("[%s]\n", __func__);
+	EnterIdleScreen();
 }
 
 void EnterPoweroffScreen(void)
@@ -1712,43 +1725,58 @@ void EnterPoweroffScreen(void)
 	screen_id = SCREEN_ID_POWEROFF;	
 	scr_msg[SCREEN_ID_POWEROFF].act = SCREEN_ACTION_ENTER;
 	scr_msg[SCREEN_ID_POWEROFF].status = SCREEN_STATUS_CREATING;		
+}
 
-	Key_Event_register_Handler(poweroff_rightkeyfunc, poweroff_rightkeyfunc);
+void PowerOffShowStatus(void)
+{
+	u16_t x,y,w,h;
+	u8_t str_title[] = "POWER OFF";
+	
+	LCD_DrawRectangle(PWR_OFF_NOTIFY_RECT_X, PWR_OFF_NOTIFY_RECT_Y, PWR_OFF_NOTIFY_RECT_W, PWR_OFF_NOTIFY_RECT_H);
+	LCD_Fill(PWR_OFF_NOTIFY_RECT_X+1, PWR_OFF_NOTIFY_RECT_Y+1, PWR_OFF_NOTIFY_RECT_W-1, PWR_OFF_NOTIFY_RECT_H-1, BLACK);
+	
+	LCD_SetFontSize(FONT_SIZE_16);
+	LCD_MeasureString(str_title, &w, &h);
+	x = (w > (PWR_OFF_NOTIFY_RECT_W-2*PWR_OFF_NOTIFY_OFFSET_W))? 0 : ((PWR_OFF_NOTIFY_RECT_W-2*PWR_OFF_NOTIFY_OFFSET_W)-w)/2;
+	x += (PWR_OFF_NOTIFY_RECT_X+PWR_OFF_NOTIFY_OFFSET_W);
+	y = PWR_OFF_NOTIFY_RECT_Y+2;
+	LCD_ShowString(x,y,str_title);
+
+	ShowStringsInRect(PWR_OFF_NOTIFY_STRING_X, 
+					  PWR_OFF_NOTIFY_STRING_Y, 
+					  PWR_OFF_NOTIFY_STRING_W, 
+					  PWR_OFF_NOTIFY_STRING_H, 
+					  FONT_SIZE_16, 
+					  "Are you sure you want to turn it off?");
+
+	LCD_DrawRectangle(PWR_OFF_NOTIFY_YES_X, PWR_OFF_NOTIFY_YES_Y, PWR_OFF_NOTIFY_YES_W, PWR_OFF_NOTIFY_YES_H);
+	LCD_MeasureString("SOS(Y)", &w, &h);
+	x = PWR_OFF_NOTIFY_YES_X+(PWR_OFF_NOTIFY_YES_W-w)/2;
+	y = PWR_OFF_NOTIFY_YES_Y+(PWR_OFF_NOTIFY_YES_H-h)/2;	
+	LCD_ShowString(x,y,"SOS(Y)");
+
+	LCD_DrawRectangle(PWR_OFF_NOTIFY_NO_X, PWR_OFF_NOTIFY_NO_Y, PWR_OFF_NOTIFY_NO_W, PWR_OFF_NOTIFY_NO_H);
+	LCD_MeasureString("PWR(N)", &w, &h);
+	x = PWR_OFF_NOTIFY_NO_X+(PWR_OFF_NOTIFY_NO_W-w)/2;
+	y = PWR_OFF_NOTIFY_NO_Y+(PWR_OFF_NOTIFY_NO_H-h)/2;	
+	LCD_ShowString(x,y,"PWR(N)");
+
+	Key_Event_register_Handler(poweroff_leftkeyfunc, poweroff_rightkeyfunc);
+#ifdef CONFIG_TOUCH_SUPPORT
+	register_touch_event_handle(TP_EVENT_SINGLE_CLICK, PWR_OFF_NOTIFY_YES_X, PWR_OFF_NOTIFY_YES_X+PWR_OFF_NOTIFY_YES_W, PWR_OFF_NOTIFY_YES_Y, PWR_OFF_NOTIFY_YES_Y+PWR_OFF_NOTIFY_YES_H, poweroff_leftkeyfunc);
+	register_touch_event_handle(TP_EVENT_SINGLE_CLICK, PWR_OFF_NOTIFY_NO_X, PWR_OFF_NOTIFY_NO_X+PWR_OFF_NOTIFY_NO_W, PWR_OFF_NOTIFY_NO_Y, PWR_OFF_NOTIFY_NO_Y+PWR_OFF_NOTIFY_NO_H, poweroff_rightkeyfunc);
+#endif
 }
 
 void PowerOffScreenProcess(void)
 {
-	
-	u16_t rect_x,rect_y,rect_w=180,rect_h=120;
-	u16_t x,y,w,h;
-	u8_t notify[128] = "power off?";
-	
 	switch(scr_msg[SCREEN_ID_POWEROFF].act)
 	{
 	case SCREEN_ACTION_ENTER:
 		scr_msg[SCREEN_ID_POWEROFF].act = SCREEN_ACTION_NO;
 		scr_msg[SCREEN_ID_POWEROFF].status = SCREEN_STATUS_CREATED;
 
-	#ifdef FONT_24
-		LCD_SetFontSize(FONT_SIZE_24);
-	#else
-		LCD_SetFontSize(FONT_SIZE_16);
-	#endif
-	
-		rect_x = (LCD_WIDTH-rect_w)/2;
-		rect_y = (LCD_HEIGHT-rect_h)/2;
-		LCD_DrawRectangle(rect_x, rect_y, rect_w, rect_h);
-		LCD_Fill(rect_x+1, rect_y+1, rect_w-2, rect_h-2, BLACK);
-		
-		LCD_MeasureString(notify,&w,&h);
-		x = (w > rect_w)? 0 : (rect_w-w)/2;
-		y = (h > rect_h)? 0 : (rect_h-h)/2;
-		x += rect_x;
-		y += rect_y;
-		LCD_ShowString(60,80,notify);
-
-		LCD_ShowString(40,130,"PWR(Y)");
-		LCD_ShowString(130,130,"SOS(N)");
+		PowerOffShowStatus();
 		break;
 		
 	case SCREEN_ACTION_UPDATE:

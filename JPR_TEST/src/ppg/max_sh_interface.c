7b001fd931c582aaf50ed04e82ab939b7180cb42
xiebiao <biao.x@augustint.com>
2021-09-08 13:54:38 +0800

    1.增加了PPG通讯重试的次数，防止通讯失败。 2.增加了PPG当中86176的电源开关控制，以节省功耗。

--- JPR_TEST/src/ppg/max_sh_interface.c
+++ JPR_TEST/src/ppg/max_sh_interface.c
@@ -61,7 +61,7 @@ LOG_MODULE_REGISTER(max_sh_interface, CONFIG_LOG_DEFAULT_LEVEL);
 #define EDGE (GPIO_INT_EDGE | GPIO_INT_DOUBLE_EDGE)
 
 #define MFIO_LOW_DURATION        550
-#define SS_DEFAULT_RETRIES       ((int) (4))
+#define SS_DEFAULT_RETRIES       ((int) (5))
 //max size is used for bootloader page loading
 #define SS_TX_BUF_SIZE		(BL_MAX_PAGE_SIZE+BL_AES_AUTH_SIZE+BL_FLASH_CMD_LEN)
 
@@ -88,8 +88,6 @@ void SH_Power_On(void)
 	//PPG供电打开
 	gpio_pin_configure(gpio_ppg, PPG_EN_PIN, GPIO_DIR_OUT);
 	gpio_pin_write(gpio_ppg, PPG_EN_PIN, 1);
-
-	wait_ms(50);
 }
 
 void SH_Power_Off(void)
@@ -109,13 +107,6 @@ static void sh_init_i2c(void)
 	else
 	{
 		i2c_configure(i2c_ppg, I2C_SPEED_SET(I2C_SPEED_FAST));
-		
-		LOG_INF("Value of NRF_TWIM1_NS->PSEL.SCL: %ld \n",NRF_TWIM1_NS->PSEL.SCL);
-		LOG_INF("Value of NRF_TWIM1_NS->PSEL.SDA: %ld \n",NRF_TWIM1_NS->PSEL.SDA);
-		LOG_INF("Value of NRF_TWIM1_NS->FREQUENCY: %ld \n",NRF_TWIM1_NS->FREQUENCY);
-		LOG_INF("26738688 -> 100k\n");
-		LOG_INF("67108864 -> 250k\n");
-		LOG_INF("104857600 -> 400k\n");	
 	}
 }
 
@@ -1033,6 +1024,9 @@ bool sh_init_interface(void)
 	if(s32_status != SS_SUCCESS)
 	{
 		LOG_INF("Read MCU type fail, %x \n", s32_status);
+
+		SH_Power_Off();
+		//Set_PPG_Power_Off();
 		return false;
 	}
 	LOG_INF("MCU type = %d \n", u8_rxbuf[0]);
@@ -1042,6 +1036,9 @@ bool sh_init_interface(void)
 	if (s32_status != SS_SUCCESS)
 	{
 		LOG_INF("read FW version fail %x \n", s32_status);
+
+		SH_Power_Off();
+		//Set_PPG_Power_Off();
 		return false;
 	}
 	else
@@ -1049,15 +1046,15 @@ bool sh_init_interface(void)
 		LOG_INF("FW version is %d.%d.%d \n", u8_rxbuf[0], u8_rxbuf[1], u8_rxbuf[2]);
 	}
 
-	if((mcu_type != 1) || (u8_rxbuf[2] == 0))
+	if((mcu_type != 1) || (u8_rxbuf[2] == 1))
 	{
 		NotifyShowStrings((LCD_WIDTH-180)/2, (LCD_HEIGHT-120)/2, 180, 120, FONT_SIZE_16, "PPG is upgrading firmware, please wait a few minutes!");
 		SH_OTA_upgrade_process();
 		LCD_SleepOut();
 	}
 
+	SH_Power_Off();
 	//Set_PPG_Power_Off();
-	//SH_Power_Off();
 	
 	return true;
 }

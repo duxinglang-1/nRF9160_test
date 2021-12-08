01a83d889eea6a2578c32cc4b0daf457a89e17a4
xiebiao <biao.x@augustint.com>
2021-12-08 11:57:21 +0800

    修改睡眠监测数据只有在下次启动的时候才清空，前次测量数据将一直保存。

--- AURI_TEST/src/imu/sleep.c
+++ AURI_TEST/src/imu/sleep.c
@@ -92,76 +92,82 @@ void Set_Gsensor_data(signed short x, signed short y, signed short z, int step, 
 
 		if((hour>=SLEEP_TIME_START)||(hour<SLEEP_TIME_END))  /*输入时间是 24小时制 ，监测时间段晚上8点到早上8点 */
 		{
-			if((hour>=6)&&(hour<=SLEEP_TIME_END)&&(waggle_flag==0))
+			if((hour==SLEEP_TIME_START)&&(minute==0))//正式启动当天睡眠监测，清空上一天的数据
 			{
-				for(i=0;i<6;i++)	
-				{
-					if(waggle_level[i]==0)
-						waggle_flag++; /* 计算晚上0点到6点几个小时是佩戴状态 */
-				}
-			}
-
-			if((watch_state >= (3600*2))||(waggle_flag >= 7)||/* (3)晚上2个3时终端没有动，判断为晚上睡觉没有佩戴过，累计睡眠、久坐数据清空*/
-				((hour<8)&&(sedentary_time_temp > 180)))  /* 晚上睡眠监测时间累加150分钟又走动或者久坐，默认睡眠监测数据为0*/
-			{
-				sedentary_time_temp = 0;
-				light_sleep_time = 0;				 
-				deep_sleep_time = 0;
-
+				move = 0;	 
+				gsensor = 0;
+				rtc_sec = 0;
+				move_flag = 0;
+				watch_state = 0;
+				waggle_flag = 0;
+				waggle_flag = 0;
+				sedentary_time_temp = 0;			
+				memset(waggle_level,0,sizeof(waggle_level)); /* 晃动等级 */
+				
 				last_light_sleep = 0;
 				last_deep_sleep = 0;
+				light_sleep_time = 0;
+				deep_sleep_time = 0;
 				g_light_sleep = 0;
 				g_deep_sleep = 0;
+
+				save_flag = true;
 			}
-			else if((move == step)&&(watch_state >= 60)) /* 在期间没有走动过 */
+			else
 			{
-				if(is_wearing())	//xb add 2021-03-02 脱腕之后不计算睡眠
+				if((hour>=6)&&(hour<=SLEEP_TIME_END)&&(waggle_flag==0))
 				{
-					if(watch_state >= (60*10))/* 晚上15分钟以上未晃动过，判断为深度睡眠*/
-						deep_sleep_time++; 
-					else
-						light_sleep_time++;
+					for(i=0;i<6;i++)	
+					{
+						if(waggle_level[i]==0)
+							waggle_flag++; /* 计算晚上0点到6点几个小时是佩戴状态 */
+					}
 				}
 
-				g_light_sleep = light_sleep_time + last_light_sleep;
-				g_deep_sleep = deep_sleep_time + last_deep_sleep;
+				if((watch_state >= (3600*2))||(waggle_flag >= 7)||/* (3)晚上2个3时终端没有动，判断为晚上睡觉没有佩戴过，累计睡眠、久坐数据清空*/
+					((hour<8)&&(sedentary_time_temp > 180)))  /* 晚上睡眠监测时间累加150分钟又走动或者久坐，默认睡眠监测数据为0*/
+				{
+					sedentary_time_temp = 0;
+
+					last_light_sleep = 0;
+					last_deep_sleep = 0;
+					light_sleep_time = 0;				 
+					deep_sleep_time = 0;
+					g_light_sleep = 0;
+					g_deep_sleep = 0;
+
+					save_flag = true;
+				}
+				else if((move == step)&&(watch_state >= 60)) /* 在期间没有走动过 */
+				{
+					if(is_wearing())	//xb add 2021-03-02 脱腕之后不计算睡眠
+					{
+						if(watch_state >= (60*10))/* 晚上15分钟以上未晃动过，判断为深度睡眠*/
+							deep_sleep_time++; 
+						else
+							light_sleep_time++;
+					}
+
+					g_light_sleep = light_sleep_time + last_light_sleep;
+					g_deep_sleep = deep_sleep_time + last_deep_sleep;
+
+					save_flag = true;
+				}
 			}
 
-			save_flag = true;
-		}
-		else if((hour==SLEEP_TIME_START)&&(minute==0))
-		{
-			move = 0;	 
-			gsensor = 0;
-			rtc_sec = 0;
-			move_flag = 0;
-			watch_state = 0;
-			waggle_flag = 0;
-			waggle_flag = 0;
-			
-			last_light_sleep = 0;
-			last_deep_sleep = 0;
-			light_sleep_time = 0;
-			deep_sleep_time = 0;
-			
-			sedentary_time_temp = 0;			
-			memset(waggle_level,0,sizeof(waggle_level)); /* 晃动等级 */
-
-			save_flag = true;
-		}
-
-		if(save_flag)
-		{
-			last_sport.timestamp.year = date_time.year;
-			last_sport.timestamp.month = date_time.month; 
-			last_sport.timestamp.day = date_time.day;
-			last_sport.timestamp.hour = date_time.hour;
-			last_sport.timestamp.minute = date_time.minute;
-			last_sport.timestamp.second = date_time.second;
-			last_sport.timestamp.week = date_time.week;
-			last_sport.deep_sleep = g_deep_sleep;
-			last_sport.light_sleep = g_light_sleep;
-			save_cur_sport_to_record(&last_sport);			
+			if(save_flag)
+			{
+				last_sport.timestamp.year = date_time.year;
+				last_sport.timestamp.month = date_time.month; 
+				last_sport.timestamp.day = date_time.day;
+				last_sport.timestamp.hour = date_time.hour;
+				last_sport.timestamp.minute = date_time.minute;
+				last_sport.timestamp.second = date_time.second;
+				last_sport.timestamp.week = date_time.week;
+				last_sport.deep_sleep = g_deep_sleep;
+				last_sport.light_sleep = g_light_sleep;
+				save_cur_sport_to_record(&last_sport);			
+			}
 		}	
 	}
 

86510355930325f258ca69a0b88dddc008b80a5f
xiebiao <biao.x@augustint.com>
2022-05-12 09:51:23 +0800

    修改软件版本号。

--- E2_TEST/src/settings/settings.c
+++ E2_TEST/src/settings/settings.c
@@ -21,7 +21,7 @@ bool need_reset_settings = false;
 bool need_reset_bk_level = false;
 bool need_fw_update = true;
 
-u8_t g_fw_version[64] = "V1.6.4_20220429";
+u8_t g_fw_version[64] = "V1.6.5_20220512";
 RESET_STATUS g_reset_status = RESET_STATUS_IDLE;
 
 static bool reset_redraw_flag = false;

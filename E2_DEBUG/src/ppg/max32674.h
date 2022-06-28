ad86a964522ab30e268accd98c94ef07a687f768
xiebiao <biao.x@augustint.com>
2022-06-28 14:36:55 +0800

    优化定时检测和手动检测冲突时处理。

--- E2_DEBUG/src/ppg/max32674.h
+++ E2_DEBUG/src/ppg/max32674.h
@@ -15,7 +15,7 @@
 
 #define PPG_CHECK_HR_TIMELY			1
 #define PPG_CHECK_SPO2_TIMELY		5
-#define PPG_CHECK_BPT_TIMELY		2
+#define PPG_CHECK_BPT_TIMELY		3
 
 #define PPG_HR_MAX		150
 #define PPG_HR_MIN		30

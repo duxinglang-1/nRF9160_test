29ccf3708b784cd9580cade6fb4cf4bcaed1f8dd
xiebiao <biao.x@augustint.com>
2022-10-28 10:43:16 +0800

    修改FOTA升级之后对UI等资源数据升级的启动条件判断。

--- E2_V1_NCS1.9.0_TEST/src/fota/fota_mqtt.c
+++ E2_V1_NCS1.9.0_TEST/src/fota/fota_mqtt.c
@@ -413,23 +413,22 @@ void FotaMsgProc(void)
 	if(fota_reboot_flag)
 	{
 		fota_reboot_flag = false;
-	#if 1	
-		if(strcmp(g_new_ui_ver,g_ui_ver) != 0)
+
+		if((strcmp(g_new_ui_ver,g_ui_ver) != 0) && (strlen(g_new_ui_ver) > 0))
 		{
 			dl_img_start();
 		}
-		else if(strcmp(g_new_font_ver,g_font_ver) != 0)
+		else if((strcmp(g_new_font_ver,g_font_ver) != 0) && (strlen(g_new_font_ver) > 0))
 		{
 			dl_font_start();
 		}
 	#ifdef CONFIG_PPG_SUPPORT
-		else if(strcmp(g_new_ppg_ver,g_ppg_ver) != 0)
+		else if((strcmp(g_new_ppg_ver,g_ppg_algo_ver) != 0) && (strlen(g_new_ppg_ver) > 0))
 		{
 			dl_ppg_start();
 		}
 	#endif
-		else
-	#endif		
+		else	
 		{
 			LCD_Clear(BLACK);
 			sys_reboot(1);

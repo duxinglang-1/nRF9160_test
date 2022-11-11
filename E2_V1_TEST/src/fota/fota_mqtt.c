16ada95ce8c8919f967a75c0af1271964260ff23
xiebiao <biao.x@augustint.com>
2022-11-11 16:23:02 +0800

    修改FOTA失败后的处理。

--- E2_V1_TEST/src/fota/fota_mqtt.c
+++ E2_V1_TEST/src/fota/fota_mqtt.c
@@ -169,6 +169,7 @@ static void app_dfu_transfer_start(struct k_work *unused)
 		fota_run_flag = false;
 		fota_cur_status = FOTA_STATUS_ERROR;
 		fota_redraw_pro_flag = true;
+		k_timer_start(&fota_timer, K_SECONDS(FOTA_RESULT_NOTIFY_TIMEOUT), NULL);
 	}
 }
 
@@ -266,6 +267,7 @@ void fota_dl_handler(const struct fota_download_evt *evt)
 		LOGD("Received error");
 	#endif
 		fota_cur_status = FOTA_STATUS_ERROR;
+		k_timer_start(&fota_timer, K_SECONDS(FOTA_RESULT_NOTIFY_TIMEOUT), NULL);
 		break;
 
 	case FOTA_DOWNLOAD_EVT_PROGRESS:

bd3e1e3b04a4cffc9edc124424dca621f72cc601
xiebiao <biao.x@augustint.com>
2023-01-06 19:09:22 +0800

    修改数据下载凑足4096之后再写入falsh.

--- E2_V2_NCS1.9.0_TEST/src/data_download/data_download.c
+++ E2_V2_NCS1.9.0_TEST/src/data_download/data_download.c
@@ -20,11 +20,15 @@
 
 #define TLS_SEC_TAG 42
 #define DL_SOCKET_RETRIES 2
+#define DL_BUF_LEN	4096
 
 static bool dl_start_flag = false;
 static bool dl_run_flag = false;
 static bool dl_reboot_flag = false;
 static bool dl_redraw_pro_flag = false;
+
+static uint8_t databuf[DL_BUF_LEN] = {0};
+static uint32_t datalen = 0;
 
 static dl_callback_t callback;
 static struct download_client   dlc;
@@ -111,6 +115,7 @@ static int download_client_callback(const struct download_client_evt *event)
 	case DOWNLOAD_CLIENT_EVT_FRAGMENT:
 		if(first_fragment)
 		{
+			datalen = 0;
 			err = download_client_file_size_get(&dlc, &file_size);
 			if(err != 0)
 			{
@@ -147,15 +152,23 @@ static int download_client_callback(const struct download_client_evt *event)
 			}
 		}
 
-		err = dl_target_write(event->fragment.buf, event->fragment.len);
-		if(err != 0)
+		memcpy(&databuf[datalen], event->fragment.buf, event->fragment.len);
+		datalen += event->fragment.len;
+		LOGD("FRAGMENT datalen:%d", datalen);
+		if(datalen >= DL_BUF_LEN)
 		{
-			LOGD("dl_target_write error %d", err);
-			(void) download_client_disconnect(&dlc);
-			send_evt(DOWNLOAD_EVT_ERROR);
-			return err;
+			err = dl_target_write(databuf, datalen);
+			datalen = 0;
+			
+			if(err != 0)
+			{
+				LOGD("dl_target_write error %d", err);
+				(void) download_client_disconnect(&dlc);
+				send_evt(DOWNLOAD_EVT_ERROR);
+				return err;
+			}
 		}
-
+		
 		if(!first_fragment)
 		{
 			err = dl_target_offset_get(&offset);
@@ -179,6 +192,21 @@ static int download_client_callback(const struct download_client_evt *event)
 		break;
 
 	case DOWNLOAD_CLIENT_EVT_DONE:
+		LOGD("DONE datalen:%d", datalen);
+		if(datalen > 0)
+		{
+			err = dl_target_write(databuf, datalen);
+			datalen = 0;
+			
+			if(err != 0)
+			{
+				LOGD("dl_target_write error %d", err);
+				(void) download_client_disconnect(&dlc);
+				send_evt(DOWNLOAD_EVT_ERROR);
+				return err;
+			}
+		}
+		
 		err = dl_target_done(true);
 		if(err != 0)
 		{
@@ -515,6 +543,22 @@ void dl_start(void)
 
 void dl_start_confirm(void)
 {
+#ifdef CONFIG_ANIMATION_SUPPORT	
+	AnimaStopShow();
+#endif
+#ifdef CONFIG_TEMP_SUPPORT
+	if(TempIsWorking()&&!TempIsWorkingTiming())
+		MenuStopTemp();
+#endif
+#ifdef CONFIG_PPG_SUPPORT
+	if(IsInPPGScreen()&&!PPGIsWorkingTiming())
+		MenuStopPPG();
+#endif
+#ifdef CONFIG_WIFI_SUPPORT
+	if(wifi_is_working())
+		MenuStopWifi();
+#endif
+
 	dl_cur_status = DL_STATUS_LINKING;
 	dl_redraw_pro_flag = true;
 

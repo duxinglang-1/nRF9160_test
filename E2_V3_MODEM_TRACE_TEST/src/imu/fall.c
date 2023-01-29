d24c2e3c5449d3ed742ab870937a24360856a367
xiebiao <biao.x@augustint.com>
2023-01-29 08:59:47 +0800

    创建抓取modem trace的测试工程。

---
+++ E2_V3_MODEM_TRACE_TEST/src/imu/fall.c
@@ -0,0 +1,306 @@
+/****************************************Copyright (c)************************************************
+** File Name:				fall.c
+** Descriptions:			sfallos message process source file
+** Created By:				xie biao
+** Created Date:			2021-09-24
+** Modified Date:			2021-09-24 
+** Version:			    	V1.0
+******************************************************************************************************/
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <zephyr.h>
+#include <soc.h>
+#include <device.h>
+#include <nrf_socket.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
+#include <nrfx.h>
+#include "settings.h"
+#include "sos.h"
+#include "Max20353.h"
+#include "Alarm.h"
+#include "lcd.h"
+#include "key.h"
+#include "gps.h"
+#include "screen.h"
+#include "fall.h"
+#ifdef CONFIG_WIFI_SUPPORT
+#include "esp8266.h"
+#endif
+#include "logger.h"
+
+FALL_STATUS fall_state = FALL_STATUS_IDLE;
+
+static bool fall_trigger_flag = false;
+static bool fall_send_flag = false;
+static bool fall_end_flag = false;
+static bool fall_start_gps_flag = false;
+
+uint8_t fall_trigger_time[16] = {0};
+
+static void FallTimerOutCallBack(struct k_timer *timer_id);
+K_TIMER_DEFINE(fall_timer, FallTimerOutCallBack, NULL);
+static void FallStartGPSCallBack(struct k_timer *timer_id);
+K_TIMER_DEFINE(fall_gps_timer, FallStartGPSCallBack, NULL);
+
+static void FallEnd(void)
+{
+#ifdef CONFIG_ANIMATION_SUPPORT
+	AnimaStopShow();
+#endif
+
+#ifdef CONFIG_AUDIO_SUPPORT	
+	FallStopAlarm();
+#endif
+	k_timer_stop(&fall_timer);
+
+	fall_state = FALL_STATUS_IDLE;
+	LCD_Set_BL_Mode(LCD_BL_ALWAYS_ON);
+	EnterIdleScreen();
+}
+
+static void FallTimerOutCallBack(struct k_timer *timer_id)
+{
+	if(screen_id == SCREEN_ID_FALL)
+	{
+		switch(fall_state)
+		{
+		case FALL_STATUS_IDLE:
+			break;
+			
+		case FALL_STATUS_NOTIFY:
+			fall_state = FALL_STATUS_SENDING;
+			fall_send_flag = true;
+			ClearAllKeyHandler();
+			k_timer_start(&fall_timer, K_SECONDS(FALL_SENDING_TIMEOUT), NULL);
+			break;
+		
+		case FALL_STATUS_SENDING:
+			fall_state = FALL_STATUS_SENT;
+			k_timer_start(&fall_timer, K_SECONDS(FALL_SEND_OK_TIMEOUT), NULL);
+			break;
+			
+		case FALL_STATUS_SENT:
+			fall_end_flag = true;
+			break;
+			
+		case FALL_STATUS_CANCEL:
+			fall_end_flag = true;
+			break;
+
+		case FALL_STATUS_CANCELED:
+			break;
+		}
+		
+		scr_msg[SCREEN_ID_FALL].act = SCREEN_ACTION_UPDATE;
+	}
+}
+
+void FallStartGPSCallBack(struct k_timer *timer_id)
+{
+	fall_start_gps_flag = true;
+}
+
+#ifdef CONFIG_WIFI_SUPPORT
+void fall_get_wifi_data_reply(wifi_infor wifi_data)
+{
+	uint8_t reply[256] = {0};
+	uint32_t count=3,i;
+
+	if(wifi_data.count > 0)
+		count = wifi_data.count;
+
+	strcat(reply, "3,");
+	for(i=0;i<count;i++)
+	{
+		strcat(reply, wifi_data.node[i].mac);
+		strcat(reply, "&");
+		strcat(reply, wifi_data.node[i].rssi);
+		strcat(reply, "&");
+		if(i < (count-1))
+			strcat(reply, "|");
+	}
+
+	NBSendFallWifiData(reply, strlen(reply));
+}
+#endif
+
+void fall_get_gps_data_reply(bool flag, struct nrf_modem_gnss_pvt_data_frame gps_data)
+{
+	uint8_t reply[128] = {0};
+	uint8_t tmpbuf[8] = {0};
+	uint32_t tmp1;
+	double tmp2;
+
+	if(!flag)
+		return;
+	
+	//latitude
+	if(gps_data.latitude < 0)
+	{
+		strcat(reply, "-");
+		gps_data.latitude = -gps_data.latitude;
+	}
+
+	tmp1 = (uint32_t)(gps_data.latitude);	//经度整数部分
+	tmp2 = gps_data.latitude - tmp1;		//经度小数部分
+	//integer
+	sprintf(tmpbuf, "%d", tmp1);
+	strcat(reply, tmpbuf);
+	//dot
+	strcat(reply, ".");
+	//decimal
+	tmp1 = (uint32_t)(tmp2*1000000);
+	sprintf(tmpbuf, "%02d", (uint8_t)(tmp1/10000));
+	strcat(reply, tmpbuf);
+	tmp1 = tmp1%10000;
+	sprintf(tmpbuf, "%02d", (uint8_t)(tmp1/100));
+	strcat(reply, tmpbuf);	
+	tmp1 = tmp1%100;
+	sprintf(tmpbuf, "%02d", (uint8_t)(tmp1));
+	strcat(reply, tmpbuf);
+
+	//semicolon
+	strcat(reply, ";");
+	
+	//longitude
+	if(gps_data.longitude < 0)
+	{
+		strcat(reply, "-");
+		gps_data.longitude = -gps_data.longitude;
+	}
+
+	tmp1 = (uint32_t)(gps_data.longitude);	//经度整数部分
+	tmp2 = gps_data.longitude - tmp1;	//经度小数部分
+	//integer
+	sprintf(tmpbuf, "%d", tmp1);
+	strcat(reply, tmpbuf);
+	//dot
+	strcat(reply, ".");
+	//decimal
+	tmp1 = (uint32_t)(tmp2*1000000);
+	sprintf(tmpbuf, "%02d", (uint8_t)(tmp1/10000));
+	strcat(reply, tmpbuf);	
+	tmp1 = tmp1%10000;
+	sprintf(tmpbuf, "%02d", (uint8_t)(tmp1/100));
+	strcat(reply, tmpbuf);	
+	tmp1 = tmp1%100;
+	sprintf(tmpbuf, "%02d", (uint8_t)(tmp1));
+	strcat(reply, tmpbuf);
+
+	//semicolon
+	strcat(reply, ";");
+
+	//sos trigger time
+	strcat(reply, fall_trigger_time);
+
+	//semicolon
+	strcat(reply, ";");
+	
+	NBSendFallGpsData(reply, strlen(reply));
+}
+
+void FallAlarmCancel(void)
+{
+	if(fall_state == FALL_STATUS_NOTIFY)
+	{
+	#ifdef CONFIG_ANIMATION_SUPPORT
+		AnimaStopShow();
+	#endif
+	#ifdef CONFIG_AUDIO_SUPPORT
+		FallStopAlarm();
+	#endif
+		fall_state = FALL_STATUS_CANCEL;
+		k_timer_start(&fall_timer, K_SECONDS(FALL_CANCEL_TIMEOUT), NULL);
+
+		scr_msg[SCREEN_ID_FALL].act = SCREEN_ACTION_UPDATE;
+	}
+}
+
+void FallStart(void)
+{
+	fall_trigger_flag = true;
+}
+
+void FallAlarmStart(void)
+{
+	if(FallIsRunning())
+		return;
+
+	fall_state = FALL_STATUS_NOTIFY;
+	LCD_Set_BL_Mode(LCD_BL_ALWAYS_ON);
+	EnterFallScreen();
+
+#ifdef CONFIG_AUDIO_SUPPORT	
+	if(global_settings.language == LANGUAGE_CHN)
+		FallPlayAlarmCn();
+	else
+		FallPlayAlarmEn();
+#endif
+
+	GetSystemTimeSecString(fall_trigger_time);
+
+	SetLeftKeyUpHandler(FallAlarmCancel);
+
+	k_timer_start(&fall_timer, K_SECONDS(FALL_NOTIFY_TIMEOUT), NULL);
+}
+
+void FallAlarmSend(void)
+{
+	uint8_t delay;
+	
+#ifdef CONFIG_WIFI_SUPPORT
+	fall_wait_wifi = true;
+	APP_Ask_wifi_data();
+#endif
+
+	if(nb_is_connected())
+		delay = 30;
+	else
+		delay = 90;
+
+	k_timer_start(&fall_gps_timer, K_SECONDS(delay), NULL);
+}
+
+bool FallIsRunning(void)
+{
+	if(fall_state > FALL_STATUS_IDLE)
+	{
+		LOGD("true");
+		return true;
+	}
+	else
+	{
+		LOGD("false");
+		return false;
+	}
+}
+
+void FallMsgProcess(void)
+{
+	if(fall_trigger_flag)
+	{
+		FallAlarmStart();
+		fall_trigger_flag = false;
+	}
+
+	if(fall_start_gps_flag)
+	{
+		fall_wait_gps = true;
+		APP_Ask_GPS_Data();
+		fall_start_gps_flag = false;
+	}
+	
+	if(fall_send_flag)
+	{
+		FallAlarmSend();
+		fall_send_flag = false;
+	}
+	
+	if(fall_end_flag)
+	{
+		FallEnd();
+		fall_end_flag = false;
+	}
+}
---
+++ E2_V3_MODEM_TRACE_TEST/src/imu/lsm6dso.h
@@ -0,0 +1,67 @@
+#ifdef CONFIG_IMU_SUPPORT
+
+#include <nrf9160.h>
+#include <zephyr.h>
+#include <device.h>
+#include "inner_flash.h"
+
+//单次测量
+typedef struct
+{
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hour;
+	uint8_t min;
+	uint8_t sec;
+	uint16_t steps;
+}step_rec1_data;
+
+//整点测量
+typedef struct
+{
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint16_t steps[24];
+}step_rec2_data;
+
+extern bool reset_steps;
+#ifdef CONFIG_FALL_DETECT_SUPPORT
+extern bool fall_wait_gps;
+#endif
+extern uint16_t g_last_steps;
+extern uint16_t g_steps;
+extern uint16_t g_calorie;
+extern uint16_t g_distance;
+
+extern sport_record_t last_sport;
+
+extern void IMU_init(struct k_work_q *work_q);
+extern void IMUMsgProcess(void);
+
+/*fall detection*/
+extern bool int2_event; //trigger of fall-detection
+extern bool fall_result; //fall_result = true when fall happens
+extern void fall_detection(void); //fall detection algorithm, if a fall happens, bool fall_result = true;
+
+/*wrist tilt*/
+extern void is_tilt(void); //detect if a tilt happened
+extern bool wrist_tilt; //wrist_tilt = true if a tilt happened
+extern void disable_tilt_detection(void); //disable wrist tilt detection
+extern void enable_tilt_detection(void); //enable wrist tilt detection
+
+/*step counter*/
+extern bool int1_event;
+extern void GetImuSteps(uint16_t *steps);
+extern void ReSetImuSteps(void);
+extern void UpdateIMUData(void);
+extern void GetSportData(uint16_t *steps, uint16_t *calorie, uint16_t *distance);
+extern void lsm6dso_sensitivity(void);
+
+/*sleep monitor*/
+extern void Set_Gsensor_data(signed short x, signed short y, signed short z, int step, int hr, int hour, int minute, int charging);
+extern int get_light_sleep_time(void); //return light sleep time in minutes
+extern int get_deep_sleep_time(void); //return deep sleep time in minutes
+
+#endif/*CONFIG_IMU_SUPPORT*/

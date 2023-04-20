965eac5418b6c172fe98b93c24acf64544679690
xiebiao <biao.x@augustint.com>
2023-04-20 10:40:13 +0800

    修改固件版本号。

--- E2_V3_TEST/src/settings/settings.c
+++ E2_V3_TEST/src/settings/settings.c
@@ -22,7 +22,7 @@ bool need_save_time = false;
 bool need_reset_settings = false;
 bool need_reset_bk_level = false;
 
-uint8_t g_fw_version[64] = "V3.0.6_20230412";
+uint8_t g_fw_version[64] = "V3.0.7_20230420";
 
 RESET_STATUS g_reset_status = RESET_STATUS_IDLE;
 

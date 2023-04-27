3fa88163eabdf0ba3a7f396e20905e1ffa56137e
xiebiao <biao.x@augustint.com>
2023-04-27 09:34:47 +0800

    修改固件版本号。

--- E2_V3_IMU_TEST/src/settings/settings.c
+++ E2_V3_IMU_TEST/src/settings/settings.c
@@ -22,7 +22,7 @@ bool need_save_time = false;
 bool need_reset_settings = false;
 bool need_reset_bk_level = false;
 
-uint8_t g_fw_version[64] = "V3.0.8_20230427";
+uint8_t g_fw_version[64] = "V3.0.9_20230427";
 
 RESET_STATUS g_reset_status = RESET_STATUS_IDLE;
 

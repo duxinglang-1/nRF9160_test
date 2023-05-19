902a2894661f917d9a2c6e3d0f8e294a8aa1a186
xiebiao <biao.x@augustint.com>
2023-05-19 18:50:32 +0800

    更新图片资源。

--- E2_V3_TEST/src/image/img.h
+++ E2_V3_TEST/src/image/img.h
@@ -102,14 +102,14 @@ extern unsigned char IMG_IDLE_HR_BG[8720];
 extern unsigned char IMG_IDLE_HR_ICON[888];
 //idle spo2
 extern unsigned char IMG_IDLE_SPO2_BG[8720];
-extern unsigned char IMG_IDLE_SPO2_ICON[1088];
+extern unsigned char IMG_IDLE_SPO2_ICON[872];
 //idle step
 extern unsigned char IMG_IDLE_STEP_BG[8720];
 extern unsigned char IMG_IDLE_STEP_ICON[568];
 //idle temp(3)
 extern unsigned char IMG_IDLE_TEMP_BG[8720];
-extern unsigned char IMG_IDLE_TEMP_C_ICON[968];
-extern unsigned char IMG_IDLE_TEMP_F_ICON[968];
+extern unsigned char IMG_IDLE_TEMP_C_ICON[800];
+extern unsigned char IMG_IDLE_TEMP_F_ICON[800];
 //idle net mode(2)
 extern unsigned char IMG_IDLE_NET_LTEM[344];
 extern unsigned char IMG_IDLE_NET_NB[316];

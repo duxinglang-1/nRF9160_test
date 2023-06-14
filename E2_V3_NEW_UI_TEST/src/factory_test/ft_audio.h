eef4b5d15c309eb375a3dbf9eff174cd2513a94f
xiebiao <biao.x@augustint.com>
2023-06-14 16:57:05 +0800

    删除无用的测试工程。

--- E2_V3_NEW_UI_TEST/src/factory_test/ft_audio.h
+++
@@ -1,8 +0,0 @@
-/****************************************Copyright (c)************************************************
-** File Name:			    ft_audio.h
-** Descriptions:			Factory test audio module head file
-** Created By:				xie biao
-** Created Date:			2023-02-17
-** Modified Date:      		2023-02-17 
-** Version:			    	V1.0
-******************************************************************************************************/

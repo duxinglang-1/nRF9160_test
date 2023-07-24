0dc74c516d858360e9ac757fdf4d07e0bd149a56
xiebiao <biao.x@augustint.com>
2023-07-24 18:52:46 +0800

    区分中英文caremeta的登录地址。

--- E2_V3_NCS2.3.0_TEST/src/settings/settings.h
+++ E2_V3_NCS2.3.0_TEST/src/settings/settings.h
@@ -12,7 +12,11 @@
 #define MENU_MAX_COUNT	10
 #define MENU_NAME_MAX	20
 
+#ifdef FW_FOR_CN
+#define SETTINGS_CAREMATE_URL	"https://caremate.audarhealth.cn/login"
+#else
 #define SETTINGS_CAREMATE_URL	"https://caremate.audarhealth.com/login"
+#endif
 
 typedef void(*menu_handler)(void);
 

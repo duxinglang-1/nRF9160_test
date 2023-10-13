4f4bc39736e984297448b0a31eccd3c3f6860ce4
xiebiao <biao.x@augustint.com>
2023-10-13 10:17:21 +0800

    修改其他界面电量低于7显示红色。

--- E2_V3_NCS2.3.0_TEST/src/screen/screen.c
+++ E2_V3_NCS2.3.0_TEST/src/screen/screen.c
@@ -762,7 +762,7 @@ void IdleUpdateBatSoc(void)
 		flag = true;
 		bat_charging_index = g_bat_soc/10;
 		
-		if(g_bat_soc >= 10)
+		if(g_bat_soc >= 7)
 		{
 			LCD_ShowImg_From_Flash(IDLE_BAT_X, IDLE_BAT_Y, IMG_BAT_RECT_WHITE_ADDR);
 			LCD_Fill(IDLE_BAT_INNER_RECT_X, IDLE_BAT_INNER_RECT_Y, (g_bat_soc*IDLE_BAT_INNER_RECT_W)/100, IDLE_BAT_INNER_RECT_H, GREEN);

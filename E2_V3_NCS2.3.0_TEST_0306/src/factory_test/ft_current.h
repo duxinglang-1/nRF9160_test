0757aed54e97c4d28f9401e43a66b505b2660e14
xiebiao <biao.x@augustint.com>
2024-05-15 17:24:52 +0800

    创建0306分支版本。

---
+++ E2_V3_NCS2.3.0_TEST_0306/src/factory_test/ft_current.h
@@ -0,0 +1,13 @@
+/****************************************Copyright (c)************************************************
+** File Name:			    ft_current.h
+** Descriptions:			Factory test PCBA standby current head file
+** Created By:				xie biao
+** Created Date:			2023-03-13
+** Modified Date:      		2023-03-13 
+** Version:			    	V1.0
+******************************************************************************************************/
+#ifndef __FT_CURRENT_H__
+#define __FT_CURRENT_H__
+
+
+#endif/*__FT_CURRENT_H__*/
---
+++ E2_V3_NCS2.3.0_TEST_0306/src/logger/logger.h
@@ -0,0 +1,27 @@
+/****************************************Copyright (c)************************************************
+** File Name:			    logger.h
+** Descriptions:			log message process head file
+** Created By:				xie biao
+** Created Date:			2021-10-25
+** Modified Date:      		2021-10-25 
+** Version:			    	V1.0
+******************************************************************************************************/
+#ifndef __LOGGER_H__
+#define __LOGGER_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <nrf9160.h>
+#include <zephyr/kernel.h>
+
+#define LOG_BUFF_SIZE (1024)
+/* 无自动添加函数名 */
+#define LOGC(fmt, args...) LOGDD("", fmt, ##args)
+/* 有自动添加函数名 */
+#define LOGD(fmt, args...) LOGDD(__func__, fmt, ##args)
+/* 有自动添加函数名 */
+#define LOGM(fmt, args...) LOGDM(__func__, fmt, ##args)
+
+//#define TEST_DEBUG
+
+#endif/*__LOGGER_H__*/

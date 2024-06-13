94e8ed7799d31f1883a429c185e325247758007c
xiebiao <biao.x@augustint.com>
2024-06-13 10:28:20 +0800

    删除多余的测试工程。

--- E2_V3_GPIO_FOR_I2C_TEST/src/factory_test/ft_audio.c
+++
@@ -1,9 +0,0 @@
-/****************************************Copyright (c)************************************************
-** File Name:			    ft_audio.c
-** Descriptions:			Factory test audio module source file
-** Created By:				xie biao
-** Created Date:			2023-02-17
-** Modified Date:      		2023-02-17 
-** Version:			    	V1.0
-******************************************************************************************************/
-
